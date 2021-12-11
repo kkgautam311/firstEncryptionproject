b5fb5f2759ca7ac5fc0978ad090265158b4f252b
Kamal Kant Gautam <Kamal Kant Gautam>
2021-12-11 16:00:40 +0530

    Added Hashing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -26,10 +26,6 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/",(req,res)=>{
@@ -45,16 +41,19 @@ app.get("/register",(req,res)=>{
 });
 
 app.post("/register",(req,res)=>{
-    const newUser = new User(req.body);
+    const newUser = new User({
+        username:req.body.username,
+        password:md5(req.body.password)
+    });
 
     newUser.save()
-    .then(()=>{res.render("secrets")})
+    .then(()=>{res.render("login")})
     .catch((err)=>{console.log("User already exist plz login.")})
 });
 
 app.post("/login",(req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({username:username})
     .then((doc)=>{
